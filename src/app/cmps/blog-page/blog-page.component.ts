31560a5ef48d28f570fb108e7918a15bf15bf0f0
danielle <daniellezra1@gmail.com>
2021-07-24 02:03:39 +0300

    mitzpe ramon

--- src/app/cmps/blog-page/blog-page.component.ts
+++ src/app/cmps/blog-page/blog-page.component.ts
@@ -1,41 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/app/app.service';
-import { map } from 'rxjs/operators'
-import { Post } from '../../models/post'
+import { map } from 'rxjs/operators';
+import { Post } from '../../models/post';
 
 @Component({
   selector: 'blog-page',
   templateUrl: './blog-page.component.html',
-  styleUrls: ['./blog-page.component.scss']
+  styleUrls: ['./blog-page.component.scss'],
 })
 export class BlogPageComponent implements OnInit {
   postList = [];
   selectedPostList: Post[];
 
-  constructor(private service: AppService) { }
+  constructor(private service: AppService) {}
 
   ngOnInit(): void {
-    this.service.getPosts().snapshotChanges()
-    .pipe(
-      map(changes =>
-        changes.map(c =>
-          ({ id: c.payload.doc.id, ...c.payload.doc.data() })
+    this.service
+      .getPosts()
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((c) => ({
+            id: c.payload.doc.id,
+            ...c.payload.doc.data(),
+          }))
         )
       )
-    ).subscribe(res => {
-      this.postList = [res[8], res[7], res[5], res[1], res[2]];
-      this.selectedPostList = this.postList
-    });
+      .subscribe((res) => {
+        this.postList = [res[9], res[8], res[7], res[5], res[1], res[2]];
+        this.selectedPostList = this.postList;
+      });
   }
 
   filterPostList(e) {
     if (e) {
-      this.selectedPostList = this.postList.filter(post => {
-        return post.tags?.find(tag => tag === e)
-      })
+      this.selectedPostList = this.postList.filter((post) => {
+        return post.tags?.find((tag) => tag === e);
+      });
     } else {
       this.selectedPostList = this.postList;
     }
   }
-
 }
--- src/app/cmps/home/home.component.ts
+++ src/app/cmps/home/home.component.ts
@@ -1,43 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/app/app.service';
-import { map } from 'rxjs/operators'
-import { Post } from '../../models/post'
+import { map } from 'rxjs/operators';
+import { Post } from '../../models/post';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   postList = [];
   selectedPostList: Post[];
   latestPosts: Post[];
 
-  constructor(private service: AppService) { }
+  constructor(private service: AppService) {}
 
   ngOnInit(): void {
-    this.service.getPosts().snapshotChanges()
-    .pipe(
-      map(changes =>
-        changes.map(c =>
-          ({ id: c.payload.doc.id, ...c.payload.doc.data() })
+    this.service
+      .getPosts()
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((c) => ({
+            id: c.payload.doc.id,
+            ...c.payload.doc.data(),
+          }))
         )
       )
-    ).subscribe(res => {
-      this.postList = [res[8], res[7], res[5], res[1], res[2]];
-      this.selectedPostList = this.postList
-      this.latestPosts = this.postList.filter((post, index) => index < 3)
-    });
+      .subscribe((res) => {
+        this.postList = [res[9], res[8], res[7], res[5], res[1], res[2]];
+        this.selectedPostList = this.postList;
+        this.latestPosts = this.postList.filter((post, index) => index < 3);
+      });
   }
 
   filterPostList(e) {
     if (e) {
-      this.selectedPostList = this.postList.filter(post => {
-        return post.tags.find(tag => tag === e)
-      })
+      this.selectedPostList = this.postList.filter((post) => {
+        return post.tags.find((tag) => tag === e);
+      });
     } else {
       this.selectedPostList = this.postList;
     }
   }
-
 }
